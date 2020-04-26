aba16314c8bb96d4c2befaf863c24f93081f3de7
Yury Petrov <ypetrovwork@outlook.com>
2020-04-26 19:59:39 +1000

    etl.py now has docstrings.

--- udacity/data_warehousing_cloud/Project 3 - DWH Redshift/etl.py
+++ udacity/data_warehousing_cloud/Project 3 - DWH Redshift/etl.py
@@ -6,18 +6,36 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    The function to load data from s3 to the staging tables.
+    :param cur: cursor to execute the query with.
+    :param conn: connection to commit transaction.
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    The function to insert the data from the staging tables to the star schema tables.
+    :param cur: cursor to execute the query with.
+    :param conn: connection to commit transaction.
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    The main method to run in order to populate the dwh tables for the sparkify system.
+    This is done by running a set of queries described in sql_queries.py with connection details as per dwh.cfg.
+    It is done in two steps":
+    1. Loading data from s3 to the staging tables.
+    2. Inserting data to the dwh tables from the staging tables. Any data found in the staging tables that is also
+    present in the facts/dimension tables is removed to avoid duplicates or stale state.
+    """
     print("Starting etl")
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
