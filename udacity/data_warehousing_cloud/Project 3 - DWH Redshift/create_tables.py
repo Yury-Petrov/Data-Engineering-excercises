9f06d7c56e9f7951571f5aacc15f41f544b7d5d4
Yury Petrov <ypetrovwork@outlook.com>
2020-04-26 19:55:43 +1000

    create_tables.py is now with docstrings. Removed unused function.

--- udacity/data_warehousing_cloud/Project 3 - DWH Redshift/create_tables.py
+++ udacity/data_warehousing_cloud/Project 3 - DWH Redshift/create_tables.py
@@ -1,36 +1,41 @@
 import configparser
-from typing import Dict
 
-import boto3
 import psycopg2
-from sql_queries import create_table_queries, drop_table_queries
+
 from redshift_utils import get_redshift_connection_string
+from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    The function to drop the tables for the sporkify system.
+    :param cur: cursor to execute the query with.
+    :param conn: connection to commit transaction.
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    The function to create the tables for the sporkify system.
+    :param cur: cursor to execute the query with.
+    :param conn: connection to commit transaction.
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
-def get_endpoint_info(region: str, key: str, secret: str, cluster_identifier: str) -> Dict:
-    redshift = boto3.client(
-        'redshift',
-        region_name=region,
-        aws_access_key_id=key,
-        aws_secret_access_key=secret
-    )
-
-    return redshift.describe_clusters(ClusterIdentifier=cluster_identifier)['Clusters'][0]['Endpoint']
-
-
 def main():
+    """
+    The main method to run in order to create tables for the sparkyfy system.
+    This is done by running a set of queries described in sql_queries.py with connection details as per dwh.cfg.
+    It is done in two steps":
+    1. Dropping any tables that has been created for the task.
+    2. Recreating the tables.
+    """
     print("Creating tables")
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
